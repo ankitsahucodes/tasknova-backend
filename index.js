489447e21d9feee4856d82f9ce03a0594e2fd772
Ankit Sahu <ankitsahu2829@gmail.com>
2026-02-05 21:06:43 +0530

    second commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express")
+
+const app = express()
+
+const cors = require("cors");
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
+const { initializeDatabase } = require("./db/db.connect");
+
+initializeDatabase()
