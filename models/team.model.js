ffef21874d097d21f120ba647e1aaa84bda1db68
Ankit Sahu <ankitsahu2829@gmail.com>
2026-02-17 23:51:01 +0530

    added auth routes and middleware

--- models/team.model.js
+++ models/team.model.js
@@ -5,5 +5,7 @@ const mongoose = require("mongoose");
 const teamSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true }, // Team names must be unique
   description: { type: String }, // Optional description for the team
+}, {
+  timestamps: true, 
 });
 module.exports = mongoose.model("Team", teamSchema);
